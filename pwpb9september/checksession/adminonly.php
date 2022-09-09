ec177e331336d31745976130585364e3dbeacee5
RedIrfan <89332016+RedIrfan@users.noreply.github.com>
2022-09-09 08:36:00 +0700

    Add files via upload
    because the check session had bugs

--- pwpb9september/checksession/adminonly.php
+++ pwpb9september/checksession/adminonly.php
@@ -3,5 +3,7 @@
         if ($_SESSION["user_role"] != 1){
             Header("Location:/pwpb9september/index.php");
         }
+    } else{
+        Header("Location:/pwpb9september/index.php");
     }
 ?>
